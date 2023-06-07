5c9540265585256e8e47cbcf69129df55e00402d
Michael Alan Stowe <mastowe94@gmail.com>
2023-06-07 01:49:49 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (firstTwo) {
+    return [firstTwo[0],firstTwo[1]];
+}
+
+const returnLastTwoDrivers = function (lastTwo) {
+    return [lastTwo[lastTwo.length - 2], lastTwo[lastTwo.length - 1]]
+}
+
+const selectingDrivers = function () {
+    return [returnFirstTwoDrivers,returnLastTwoDrivers]
+}()
+
+const createFareMultiplier = function(num) {
+    return function(fare) {
+        return num * fare
+    };
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arra,fun) {
+    return fun(arra);
+}
+
